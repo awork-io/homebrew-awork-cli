63ad3288b8df50a753172f6e4527b5336e79458b
Nils Czernig <nils@awork.com>
2026-02-01 20:19:51 +0100

    Update awk-cli to 0.1.3

--- Formula/awk-cli.rb
+++ Formula/awk-cli.rb
@@ -1,24 +1,24 @@
 class AwkCli < Formula
   desc "Token-only, swagger-driven CLI for awork"
   homepage "https://github.com/awork-io/awk-cli"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/awork-io/awk-cli/releases/download/v0.1.2/awork-osx-arm64.tar.gz"
-      sha256 "bc1006dfef8b4f38d2da4f2b854c265f731097f79b081ef20738a14ed66574f8"
+      url "https://github.com/awork-io/awk-cli/releases/download/v0.1.3/awork-osx-arm64.tar.gz"
+      sha256 "8a4ab18783dc4b0b26d5daedbe6631bb3dfe69f8cd80e3439496da9e7c84f487"
     end
 
     on_intel do
-      url "https://github.com/awork-io/awk-cli/releases/download/v0.1.2/awork-osx-x64.tar.gz"
-      sha256 "249b2fbeb4fdc78b9a526b9bc489e7c9e4ba26ea54bef0f5f9eea3b66e91ded2"
+      url "https://github.com/awork-io/awk-cli/releases/download/v0.1.3/awork-osx-x64.tar.gz"
+      sha256 "97574905128a6912bf3ef9daf60c0089a3c727f7c655b866e6b310df81d8ae62"
     end
   end
 
   on_linux do
-    url "https://github.com/awork-io/awk-cli/releases/download/v0.1.2/awork-linux-x64.tar.gz"
-    sha256 "896312ec7da31e019608b4622e266d033fef3b00581f986625be271c462e1381"
+    url "https://github.com/awork-io/awk-cli/releases/download/v0.1.3/awork-linux-x64.tar.gz"
+    sha256 "3258055e6852b6e9c2704ea1f78b8808e24113639a49eda33cff6805bf584722"
   end
 
   def install
